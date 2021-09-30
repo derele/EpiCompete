59ba99c617bb8624daa41a04677b7d09c8bad5f1
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2021-09-30 16:39:17 +0200

    new plots based on new infection_type variable

--- 3_plotting_challengeINF.R
+++ 3_plotting_challengeINF.R
@@ -22,6 +22,21 @@ CIMouse%>%
     theme_bw()
 dev.off()
 
+
+pdf("figures/OOcystOverview2.pdf", width=8, height=5)
+CIMouse%>% 
+    ggplot(aes(infection_type, max_OOC)) +
+    geom_boxplot() +
+    geom_jitter(width=0.2, alpha=0.8) +
+    ##    facet_wrap(~experiment) +
+    scale_y_log10()+
+    coord_flip()+
+    theme_bw()
+dev.off()
+
+
+
+
 pdf("figures/WLossOverview1.pdf", width=8, height=5)
 CIMouse%>% 
     ggplot(aes(infection_history,
@@ -29,5 +44,17 @@ CIMouse%>%
     geom_boxplot() +
     geom_jitter(width=0.2, alpha=0.8) +
     ## facet_wrap(~experiment) +
+    theme_bw()
+dev.off()
+
+
+pdf("figures/WLossOverview2.pdf", width=8, height=5)
+CIMouse%>% 
+    ggplot(aes(infection_type, max_WL)) +
+    geom_boxplot() +
+    geom_jitter(width=0.2, alpha=0.8) +
+    ##    facet_wrap(~experiment) +
+    scale_y_log10()+
+    coord_flip()+
     theme_bw()
 dev.off()
